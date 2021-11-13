a7a4ae8f4bb4589750dc5fa26f082153215579c5
dalai <dalai2123@hotmail.com>
2021-11-13 09:51:49 -0600

    Creado el repo adoptapet para produccion

---
+++ config/env.sh
@@ -0,0 +1,4 @@
+export NODE_ENV='development'
+export PORT=3000
+export SECRET='secret' # para mayor seguridad puedes cambiar esto por el secreto de tu preferencia
+export MONGODB_URI="mongodb+srv://Dalaia12:W4ninoko@cluster0.m4zgb.mongodb.net/AdoptaPet?retryWrites=true&w=majority"
